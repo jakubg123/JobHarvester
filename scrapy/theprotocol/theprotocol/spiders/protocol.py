8deb5ebb389dbe90e46db47542ca15240cd3153c
jakubg123 <jakubgniadek87@gmail.com>
2023-12-06 23:50:53 +0100

    url, title, company

--- scrapy/theprotocol/theprotocol/spiders/protocol.py
+++ scrapy/theprotocol/theprotocol/spiders/protocol.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy_playwright.page import PageMethod
 
 
 class ProtocolSpider(scrapy.Spider):
@@ -45,7 +46,58 @@ class ProtocolSpider(scrapy.Spider):
                 print(f"Wrong category input, {category_type}.")
         print(categories)
         return categories
+    
+    def build_url(self):
+        experience_part = ','.join(self.experience_categories)
+        department_part = ','.join(self.department_categories)
+        # language_part = ','.join(self.language_categories)
+        # if language_part != '':
+        url = f'{self.base_url}/filtry/{department_part};sp/{experience_part};p'
+        return url
 
+    def start_requests(self):
+        url = self.build_url()
+        yield scrapy.Request(
+            url,
+            meta=dict(
+                playwright=True,
+                playwright_include_page=True,
+                playwright_page_methods=[
+                    PageMethod("wait_for_selector",
+                               "section.lniiuf0 > div > div > div.l1785eyd > div")
+                ],
+            ),
+            callback=self.parse
+        )
 
-    def parse(self, response):
-        pass
+    async def parse(self, response):
+        if 'playwright_page' in response.meta:
+            page = response.meta['playwright_page']
+            await page.close()
+
+            for item in response.css('section.lniiuf0 > div > div > div.l1785eyd > div > a'):
+                url = item.css('::attr(href)').get()
+                job_title = item.css('#offer-title::text').get()
+                company = item.css('div.mainWrapper_mlnqt8l.GridElement_g16c3y1q > div > div > div::text').get()
+                # salary_range_element = item.css('div > div > div > div > div > span.boldText_b1wsb650::text').get()
+                # salary_range = salary_range_element if salary_range_element is not None else None
+                # must_have_main = item.css(
+                #     'div.section_s1lmyctb.GridElement_g16c3y1q > div > div > div > div > span::text').get()
+
+                # item = ProtocolSpider(
+                #     url=url,
+                #     title=job_title,
+                #     company=company,
+                #     # category=category_list,
+                #     salary_range=salary_range,
+                #     must_have_main=must_have_main,
+                #     # nice_tohave_main=nice_tohave_main,
+                # )
+
+                yield {
+                    'url' : url,
+                    'job_title': job_title,
+                    'company': company,
+                    # 'salary_range': salary_range,
+                    # must_have_main: must_have_main
+                }
