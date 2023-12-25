f938e96d74b37c31e20aef1eb5e58584e19878bd
jakubg123 <jakubgniadek87@gmail.com>
2023-12-25 15:29:24 +0100

    nofluff - mongodb pipeline

--- scrapy/nofluff/nofluff/pipelines.py
+++ scrapy/nofluff/nofluff/pipelines.py
@@ -6,8 +6,43 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pymongo
 
 
 class NofluffPipeline:
     def process_item(self, item, spider):
+        return item
+
+
+class MongoPipeline:
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        if spider.preset == 1:
+            collection_name = f"{spider.category_indicator}_{spider.language_category}_collection"
+        elif spider.preset == 2:
+            collection_name = f"{spider.category_indicator}_{spider.department_categories}_collection"
+        else:
+            collection_name = 'unknown'
+
+        collection_name = collection_name.lower().replace(" ", "_").replace(",", "_")
+
+        self.db[collection_name].insert_one(dict(item))
         return item
